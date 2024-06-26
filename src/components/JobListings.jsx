526faf19769d0dd8de751b6d23fcd0ae6bb8c1f7
JasonRavager <vergara.ecjr@gmail.com>
2024-06-26 16:02:47 +0800

    finish single page jobs

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,27 @@
 import PropTypes from "prop-types";
-import jobs from "../jobs.json";
+import { useEffect, useState } from "react";
 import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false }) => {
-  const JobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJobs();
+  }, []);
 
   function jobCard(job) {
     return <JobListing key={job.id} job={job} />;
@@ -15,9 +33,13 @@ const JobListings = ({ isHome = false }) => {
         <h2 className="mb-6 text-3xl font-bold text-center text-indigo-500">
           {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
-          {JobListings.map(jobCard)}
-        </div>
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+            {jobs.map(jobCard)}
+          </div>
+        )}
       </div>
     </section>
   );
